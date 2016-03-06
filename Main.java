1dfbd2ddfea43cc6d6403aa05c84f6d7310438e9
Freakless91 <freakless91@yandex.ru>
2016-03-06 21:51:00 +0300

    /sendmailn
    sendmail

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static com.company.Sender Sender = new com.company.Sender("Freakless91@yandex.ru", "cpne9401011036");
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Тема:");
+        String theme = br.readLine();
+        System.out.println("Текст:");
+        String text = br.readLine();
+        System.out.println("Email");
+        String email = br.readLine();
+
+        Sender.send(theme, text, email);
+    }
+}
